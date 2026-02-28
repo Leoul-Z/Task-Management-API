077a4cf90b7b344e384ef7d2749b0527220b3e41
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-02-28 21:48:41 +0300

    Delete config directory

--- config/tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TaskListCreateView, TaskRetrieveUpdateDeleteView
-
-urlpatterns = [
-    path('', TaskListCreateView.as_view(), name='task-list-create'),
-    path('<int:pk>/', TaskRetrieveUpdateDeleteView.as_view(), name='task-detail'),
-]
